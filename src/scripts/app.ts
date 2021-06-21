47bfa0cb946b14aaaffbcd70c36a247b5997c0c3
S-Mcilroy <stevenmcilroy@msn.com>
2021-06-21 21:01:43 +0100

    Added Validation for Email and Post Request

---
+++ src/scripts/app.ts
@@ -0,0 +1,39 @@
+const helpSectionContainer = document.querySelector(".help-section-conatainer");
+const errorContainer = document.querySelector(".error-container");
+const successContainer = document.querySelector(".success-container");
+const emailInputBox = document.querySelector("#input-box") as HTMLInputElement;
+const validEmailText = document.querySelector(".valid-check-text");
+
+document.querySelector(".reset-button").addEventListener("click", async () => {
+  const emailInput = emailInputBox.value;
+
+  if (validateEmail(emailInput)) {
+    let body = { email: emailInput };
+
+    console.log("BODY: ", body);
+
+    const response = await fetch(
+      "http://localhost:3005/customer/account/resetPassword",
+      {
+        method: "POST",
+        body: JSON.stringify({ email: emailInput }),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+
+    console.log("Repsonse: ", response);
+
+    if (response.status === 200) {
+      successContainer.classList.toggle("hide");
+    } else {
+      errorContainer.classList.toggle("hide");
+    }
+  } else {
+    validEmailText.classList.toggle("hide");
+  }
+});
+
+function validateEmail(email) {
+  const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(email).toLowerCase());
+}
